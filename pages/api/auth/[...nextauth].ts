628f43f460ac18040b9abfdfdd1c289c49789ae5
Rajat Balyan <balyanrajat1812@gmail.com>
2024-03-28 17:25:24 +0530

    Debugged Auth Issue

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-// import { compare } from 'bcrypt';
-import * as bcrypt from 'bcrypt';
+import { compare } from 'bcrypt';
 import prismadb from '@/lib/prismadb';
 // import { log } from "console";
 
@@ -23,34 +22,33 @@ export default NextAuth ({
             async authorize(credentials) {
                 console.log('Authorizing user with credentials...');
                 console.log(credentials);
-
-                if(!credentials?.email || credentials?.password){
-                    throw new Error('Email and password required');
+            
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Email and password are required');
                 }
-
+            
                 const user = await prismadb.user.findUnique({
                     where: {
                         email: credentials.email
                     }
                 });
-
+            
                 console.log('Found user:');
                 console.log(user);
-
-                if(!user || !user.hashedPassword) {
-                    throw new Error('Email does not exist')
+            
+                if (!user || !user.hashedPassword) {
+                    throw new Error('Invalid email or password');
                 }
-
-                const isPasswordCorrect = await bcrypt.compare(credentials.password, user.hashedPassword);
-
-                console.log('Password is correct:', isPasswordCorrect);
-
-                if(!isPasswordCorrect){
-                    throw new Error('Incorrect Password');
+            
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+            
+                if (!isCorrectPassword) {
+                    throw new Error('Invalid email or password');
                 }
-
+            
                 return user;
             }
+            
         })
     ],
     pages: {
